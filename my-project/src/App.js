c7316d8944fc5e2703c872f719fda9d30530ac8f
your name <kashiii825@gmail.com>
2024-02-27 20:19:25 +0500

    third commit

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -4,13 +4,17 @@ import Home from './Routes/Home';
 import About from './Routes/About';
 import Service from './Routes/Service';
 import Contact from './Routes/Contact';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 
 function App() {
   return (
    <div>
+    <ScrollToTop/>
     <Routes>
+      
       <Route path="/" element = {<Home/>}/>
       <Route path="/about" element = {<About/>}/>
       <Route path="/service" element = {<Service/>}/>
